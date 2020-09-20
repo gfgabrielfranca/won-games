8475f1cc30f50670761f6a63d131a653cfd75de0
Gabriel França <dev.gabrielfranca@gmail.com>
2020-09-20 18:50:58 -0300

    adding favorite button to gameCard

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -10,6 +10,11 @@ export default {
     developer: 'Rockstar Games',
     img: 'https://source.unsplash.com/user/willianjusten/300x140',
     price: 'R$ 235,00'
+  },
+  argTypes: {
+    onFav: {
+      action: 'clicked'
+    }
   }
 } as Meta
 
