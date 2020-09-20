b2c38bb4e440681937fa82143b341f20ffa7c190
Gabriel França <dev.gabrielfranca@gmail.com>
2020-09-20 16:56:14 -0300

    creating highlight component

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Read Dead it’s back',
+    subtitle: 'Come see John’s new adventures',
+    backgroundImage: '/img/red-dead-img.jpg',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2'
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
---
+++ src/components/Highlight/test.tsx
@@ -0,0 +1,36 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Highlight from '.'
+
+const props = {
+  title: 'Heading 1',
+  subtitle: 'Heading 2',
+  backgroundImage: '/img/red-dead-img.jpg',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2'
+}
+
+describe('<Highlight />', () => {
+  it('should render headings and button', () => {
+    renderWithTheme(<Highlight {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Heading 1/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Heading 2/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /Buy now/i })).toBeInTheDocument()
+  })
+
+  it('should render background image', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyle({
+      backgroundImage: `url(${props.backgroundImage})`
+    })
+  })
+})
