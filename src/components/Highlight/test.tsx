615050c0a04fb817e7ce7aa5f82df6ed4d018a72
Gabriel França <dev.gabrielfranca@gmail.com>
2020-09-20 17:25:07 -0300

    adding highlight alignment

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -2,6 +2,7 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Highlight from '.'
+import * as S from './styles'
 
 const props = {
   title: 'Heading 1',
@@ -41,5 +42,37 @@ describe('<Highlight />', () => {
       'src',
       '/float-image.png'
     )
+  })
+
+  it('should render align right by default', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatImage content'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it('should render align left', () => {
+    const { container } = renderWithTheme(
+      <Highlight {...props} alignment="left" />
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatImage'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'left', {
+      modifier: `${S.Content}`
+    })
+
+    expect(container.firstChild).toHaveStyleRule('justify-self', 'end', {
+      modifier: `${S.FloatImage}`
+    })
   })
 })
